1c3302493d8e9256914f3925bf2cf60e18e67eb9
SohaniMondal <sohanimondal040@gmail.com>
2026-04-07 18:33:37 +0530

    Initial commit

---
+++ password_checker.py
@@ -0,0 +1,35 @@
+import re
+
+def check_password_strength(password):
+    score = 0
+
+    if len(password) >= 8:
+        score += 1
+
+    if re.search(r"[A-Z]", password):
+        score += 1
+
+    if re.search(r"[a-z]", password):
+        score += 1
+
+    if re.search(r"[0-9]", password):
+        score += 1
+
+    if re.search(r'[!@#$%^&*(),.?":{}|<>]', password):
+        score += 1
+
+    if score <= 2:
+        return "Weak"
+    elif score <= 4:
+        return "Medium"
+    else:
+        return "Strong"
+
+
+while True:
+    password = input("Enter password (or 'exit' to quit): ")
+
+    if password.lower() == "exit":
+        break
+
+    print("Password Strength:", check_password_strength(password))
